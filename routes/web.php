6d0df76da8e565a45b67f1106bfe41dbb50a5f32
jramirez18 <jorramirez_18@hotmail.com>
2019-02-13 13:25:28 -0600

    Se creo el archivo TEST y se modificolos archivos welcome,  web.php

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,36 @@
 |
 */
 
+
+//ACA EL ORDEN DE LAS RUTAS SI IMPORTA MUCHO, pues primero
+//va de recorriendo de una en una
 Route::get('/', function () {
-    return view('welcome');
+    return 'welcome';
+});
+
+Route::get('/usuarios',function(){
+    return 'Usuarios';
+});
+
+route::get('/usuarios/nuevo',function(){
+    return 'Crear nuevo usuario';
+
+});
+
+route::get('/usuarios/{id}',function($id){
+    return "Mostrando detalle del usuario: {$id}";
+
+});
+
+//se le agrego un ? al 2do parametro
+route::get('/saludo/{name}/{nickname?}',function($name,$nickname=null){
+    if($nickname)
+    {
+        return "Bienvenido {$name}, tu apodo es {$nickname}";
+    
+    }
+    else{
+        return "Bienvenido {$name}, no tienes apodo";
+    }
+
 });
---
+++ tests/Feature/UserModelTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserModelTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @test 
+     */
+    function it_loads_the_users_list_page() //eliminar la palabra public
+    {
+        $this->get('/usuarios')
+        ->assertStatus(200)
+        ->assertSee('Usuarios');
+    }
+
+    /**@test */
+    function it_load_the_user_detail_page()
+    {
+        $this->get('/usuarios/5')
+        ->assertStatus(200)
+        ->assertSee('Mostrando el detalle del usuario: 5');
+    }
+
+    /** 
+     * @test
+    */
+    function it_load_the_new_user_page()
+    {
+        $this->get('/usuarios/nuevo')
+        ->assertStatus(200)
+        ->assertSee('Crear nuevo usuario');
+    }
+}
